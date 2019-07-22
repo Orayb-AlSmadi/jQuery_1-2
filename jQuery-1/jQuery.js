347f0264b893c3362c89b02a91efc8252396ae0b
orayb-alsmadi <oraybalsmadi@yahoo.com>
2019-07-22 18:17:14 +0300

    jq

--- jQuery-1/jQuery.js
+++ jQuery-1/jQuery.js
@@ -8,13 +8,37 @@ Check if jQuery is loaded
 Q2:
 If You would have very long website - How to help user with fast getting back on top :)
 */
+$("#toTop").click(function () {
+    //1 second of animation time
+    //html works for FFX but not Chrome
+    //body works for Chrome but not FFX
+    //This strange selector seems to work universally
+    $("html, body").scrollTop( 0 );
+ });
 
 
 /*
 Q3:
 If You would like to change sizes of Your div/button with on click :)
 */
-
+$( "button" )
+  .on( "click", function() {
+    $( this ).css({
+    //   "background-color": "yellow",
+    //   "font-size": "20px",
+      "height": "100px",
+      "width": "500px",
+    });
+  })
+  .on( "mouseleave", function() {
+    var styles = {
+    //   backgroundColor : "#ddd",
+    // "font-size": "15px"
+    // "height": "90px",
+    // "width": "100px",
+    };
+    $( this ).css( styles );
+  });
 
 /*
 Q4:
